2a21e5531785017ea202755f3e5e1681d689c5f8
stephen.zuniga001 <stephen.zuniga001@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-24 01:35:54 +0000

    v1.23
    git-svn-id: https://plugins.svn.wordpress.org/stripe-for-woocommerce/tags/1.23@971550 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/class-s4wc_subscriptions_gateway.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Stripe Subscription Gateway
+ *
+ * Provides a Stripe Payment Gateway for Subscriptions.
+ *
+ * @class		S4WC_Subscriptions_Gateway
+ * @extends		S4WC_Gateway
+ * @version		1.23
+ * @package		WooCommerce/Classes/Payment
+ * @author		Stephen Zuniga
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class S4WC_Subscriptions_Gateway extends S4WC_Gateway {
+
+	/**
+	 * Constructor for the gateway.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+		parent::__construct();
+
+		// Hooks
+		add_action( 'scheduled_subscription_payment_' . $this->id, array( $this, 'scheduled_subscription_payment' ), 10, 3 );
+	}
+
+	/**
+	 * Send subscription form data to Stripe
+	 * Handles sending the charge to an existing customer, a new customer (that's logged in), or a guest
+	 *
+	 * @access protected
+	 * @return boolean
+	 */
+	protected function subscription_to_stripe() {
+		global $woocommerce, $s4wc;
+
+		// Get the credit card details submitted by the form
+		$form_data = $this->get_form_data();
+
+		// If there are errors on the form, don't bother sending to Stripe.
+		if ( $form_data['errors'] == 1 ) {
+			return;
+		}
+
+		// Get customer id
+		$customer = get_user_meta( $this->current_user_id, $s4wc->settings['stripe_db_location'], true );
+
+		// Update default card
+		$default_card = $customer['cards'][ $form_data['chosen_card'] ]['id'];
+		S4WC_DB::update_customer( $this->current_user_id, array( 'default_card' => $default_card ) );
+
+		// Set up the charge for Stripe's servers
+		try {
+			$initial_payment = WC_Subscriptions_Order::get_total_initial_payment( $this->order );
+
+			$charge = $this->process_subscription_payment( $initial_payment, $this->order );
+
+			$this->transaction_id = $charge->id;
+
+			// Save data for the "Capture"
+			update_post_meta( $this->order->id, 'transaction_id', $this->transaction_id );
+			update_post_meta( $this->order->id, 'capture', strcmp( $this->charge_type, 'authorize' ) == 0 );
+
+			// Save data for cross-reference between Stripe Dashboard and WooCommerce
+			update_post_meta( $this->order->id, 'customer_id', $customer['customer_id'] );
+
+			return true;
+
+		} catch ( Exception $e ) {
+			$this->transaction_error_message = $e->getMessage();
+			wc_add_notice( __( 'Error:', 'stripe-for-woocommerce' ) . ' ' . $e->getMessage(), 'error' );
+
+			return false;
+		}
+	}
+
+	/**
+	 * Process a scheduled payment
+	 *
+	 * @access public
+	 * @param float $amount_to_charge
+	 * @param WC_Order $order
+	 * @param int $product_id
+	 * @return void
+	 */
+	public function scheduled_subscription_payment( $amount_to_charge, $order, $product_id ) {
+		$charge = $this->process_subscription_payment( $amount_to_charge, $order );
+
+		if ( $charge ) {
+			WC_Subscriptions_Manager::process_subscription_payments_on_order( $order );
+		} else {
+			WC_Subscriptions_Manager::process_subscription_payment_failure_on_order( $order, $product_id );
+		}
+	}
+
+	/**
+	 * Process the payment and return the result
+	 *
+	 * @access public
+	 * @param int $order_id
+	 * @return array
+	 */
+	public function process_payment( $order_id ) {
+		if ( WC_Subscriptions_Order::order_contains_subscription( $order_id ) ) {
+			$this->order = new WC_Order( $order_id );
+
+			if ( $this->subscription_to_stripe() ) {
+				$this->order_complete();
+
+				WC_Subscriptions_Manager::activate_subscriptions_for_order( $this->order );
+
+				$result = array(
+					'result' => 'success',
+					'redirect' => $this->get_return_url( $this->order )
+				);
+
+				return $result;
+			} else {
+				$this->payment_failed();
+				wc_add_notice( __( 'Transaction Error: Could not complete your payment.', 'stripe-for-woocommerce' ), 'error' );
+			}
+		} else {
+			return parent::process_payment( $order_id );
+		}
+	}
+
+	/**
+	 * Process the subscription payment and return the result
+	 *
+	 * @access public
+	 * @param int $amount
+	 * @param WC_Order $order
+	 * @return array
+	 */
+	public function process_subscription_payment( $amount = 0, $order ) {
+		global $s4wc;
+
+		// Can't send to stripe without a value, assume it's good to go.
+		if ( $amount === 0 ) {
+			return true;
+		}
+
+		// Set a default name, override with a subscription if it exists for Stripe's dashboard
+		$product_name = 'Subscription';
+		$order_items = $order->get_items();
+		foreach ( $order_items as $key => $item ) {
+			if ( isset( $item['subscription_status'] ) ) {
+				$product_name = $item['name'];
+				break;
+			}
+		}
+
+		// Get customer id
+		$customer = get_user_meta( $order->user_id, $s4wc->settings['stripe_db_location'], true );
+
+		// Charge description
+		$charge_description = sprintf(
+			__( 'Payment for %s (Order: %s)', 'stripe-for-woocommerce' ),
+			$product_name,
+			$order->get_order_number()
+		);
+
+		// Set up basics for charging
+		$charge_data = array(
+			'amount'		=> $amount * 100, // amount in cents
+			'currency'		=> strtolower( get_woocommerce_currency() ),
+			'description'	=> apply_filters( 's4wc_subscription_charge_description', $charge_description, $charge_description, $order ),
+			'customer'		=> $customer['customer_id'],
+			'card'			=> $customer['default_card']
+		);
+		$charge = S4WC_API::create_charge( $charge_data );
+
+		if ( isset( $charge->id ) ) {
+			$order->add_order_note( sprintf( __( 'Subscription paid (%s)', 'stripe-for-woocommerce' ), $charge->id ) );
+
+			return $charge;
+		}
+		return false;
+	}
+}
---
+++ stripe-for-woocommerce.php
@@ -0,0 +1,198 @@
+<?php
+/*
+ * Plugin Name: Stripe for WooCommerce
+ * Plugin URI: https://wordpress.org/plugins/stripe-for-woocommerce
+ * Description: Use Stripe for collecting credit card payments on WooCommerce.
+ * Version: 1.23
+ * Author: Stephen Zuniga
+ * Author URI: http://stephenzuniga.com
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Foundation built by: Sean Voss // https://github.com/seanvoss/striper
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class S4WC {
+
+	public function __construct() {
+		global $wpdb;
+
+		// Include Stripe Methods
+		include_once( 'classes/class-s4wc_api.php' );
+
+		// Include Database Manipulation Methods
+		include_once( 'classes/class-s4wc_db.php' );
+
+		// Transition to new namespace
+		if ( ! get_option( 'woocommerce_s4wc_settings' ) ) {
+
+			// Update settings
+			update_option( 'woocommerce_s4wc_settings', get_option( 'woocommerce_wc_stripe_settings', array() ) );
+			delete_option( 'woocommerce_wc_stripe_settings' );
+
+			// Update saved payment methods
+			$wpdb->query(
+				"
+				UPDATE $wpdb->postmeta
+					SET `meta_value` = 's4wc'
+					WHERE `meta_value` = 'wc_stripe'
+				"
+			);
+		}
+
+		// Grab settings
+		$this->settings = get_option( 'woocommerce_s4wc_settings', array() );
+
+		// Add default values for fresh installs
+		$this->settings['testmode']					= isset( $this->settings['testmode'] ) ? $this->settings['testmode'] : 'yes';
+		$this->settings['test_publishable_key']		= isset( $this->settings['test_publishable_key'] ) ? $this->settings['test_publishable_key'] : '';
+		$this->settings['test_secret_key']			= isset( $this->settings['test_secret_key'] ) ? $this->settings['test_secret_key'] : '';
+		$this->settings['live_publishable_key']		= isset( $this->settings['live_publishable_key'] ) ? $this->settings['live_publishable_key'] : '';
+		$this->settings['live_secret_key']			= isset( $this->settings['live_secret_key'] ) ? $this->settings['live_secret_key'] : '';
+
+		// API Info
+		$this->settings['api_endpoint']				= 'https://api.stripe.com/';
+		$this->settings['publishable_key']			= $this->settings['testmode'] == 'yes' ? $this->settings['test_publishable_key'] : $this->settings['live_publishable_key'];
+		$this->settings['secret_key']				= $this->settings['testmode'] == 'yes' ? $this->settings['test_secret_key'] : $this->settings['live_secret_key'];
+
+		// Database info location
+		$this->settings['stripe_db_location']		= $this->settings['testmode'] == 'yes' ? '_stripe_test_customer_info' : '_stripe_live_customer_info';
+
+		// Hooks
+		add_filter( 'woocommerce_payment_gateways', array( &$this, 'woocommerce_stripe_gateway' ) );
+		add_action( 'woocommerce_after_my_account', array( &$this, 'account_saved_cards' ) );
+
+		// Localization
+		load_plugin_textdomain( 'stripe-for-woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+
+	/**
+	 * Add Stripe Gateway to WooCommerces list of Gateways
+	 *
+	 * @access public
+	 * @param array $methods
+	 * @return array
+	 */
+	public function woocommerce_stripe_gateway( $methods ) {
+		if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+			return;
+		}
+
+		// Include payment gateway
+		include_once( 'classes/class-s4wc_gateway.php' );
+
+		if ( class_exists( 'WC_Subscriptions_Order' ) ) {
+			include_once( 'classes/class-s4wc_subscriptions_gateway.php' );
+
+			$methods[] = 'S4WC_Subscriptions_Gateway';
+		} else {
+			$methods[] = 'S4WC_Gateway';
+		}
+
+		return $methods;
+	}
+
+	/**
+	 * Gives front-end view of saved cards in the account page
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function account_saved_cards() {
+		s4wc_get_template( 'saved-cards.php' );
+	}
+}
+
+$GLOBALS['s4wc'] = new S4WC();
+
+/**
+ * Process the captured payment when changing order status to completed
+ *
+ * @access public
+ * @param int $order_id
+ * @return bool
+ */
+function s4wc_order_status_completed( $order_id = null ) {
+	global $woocommerce, $s4wc;
+
+	if ( ! $order_id ) {
+		$order_id = $_POST['order_id'];
+	}
+
+	$data = get_post_meta( $order_id );
+	$total = $data['_order_total'][0] * 100;
+
+	$params = array();
+	if( isset( $_POST['amount'] ) && $amount = $_POST['amount'] ) {
+		$params['amount'] = round( $amount );
+	}
+
+	if( get_post_meta( $order_id, 'capture', true ) ) {
+
+		$transaction_id = get_post_meta( $order_id, 'transaction_id', true );
+
+		$charge = S4WC_API::capture_charge( $transaction_id, $params );
+
+		return $charge;
+	}
+}
+add_action( 'woocommerce_order_status_processing_to_completed', 's4wc_order_status_completed' );
+
+/**
+ * Get error message for form validator given field name and type of error
+ *
+ * @param $field
+ * @param $type
+ * @return string
+ */
+function s4wc_get_form_error_message( $field, $type = 'undefined' ) {
+
+	if ( $type === 'invalid' ) {
+		return sprintf( __( 'Please enter a valid %s.', 'stripe-for-woocommerce' ), "<strong>$field</strong>" );
+	} else {
+		return sprintf( __( '%s is a required field.', 'stripe-for-woocommerce' ), "<strong>$field</strong>" );
+	}
+}
+
+/**
+ * Validate credit card form fields
+ *
+ * @return void
+ */
+function s4wc_validate_form() {
+	$form = array(
+		'card-number'	=> isset( $_POST['s4wc-card-number'] ) ? $_POST['s4wc-card-number'] : null,
+		'card-expiry'	=> isset( $_POST['s4wc-card-expiry'] ) ? $_POST['s4wc-card-expiry'] : null,
+		'card-cvc'		=> isset( $_POST['s4wc-card-cvc'] ) ? $_POST['s4wc-card-cvc'] : null,
+	);
+
+	if ( $form['card-number'] ) {
+		$field = __( 'Credit Card Number', 'stripe-for-woocommerce' );
+
+		wc_add_notice( s4wc_get_form_error_message( $field, $form['card-number'] ), 'error' );
+	}
+	if ( $form['card-expiry'] ) {
+		$field = __( 'Credit Card Expiration', 'stripe-for-woocommerce' );
+
+		wc_add_notice( s4wc_get_form_error_message( $field, $form['card-expiry'] ), 'error' );
+	}
+	if ( $form['card-cvc'] ) {
+		$field = __( 'Credit Card CVC', 'stripe-for-woocommerce' );
+
+		wc_add_notice( s4wc_get_form_error_message( $field, $form['card-cvc'] ), 'error' );
+	}
+}
+add_action( 'woocommerce_checkout_process', 's4wc_validate_form' );
+
+/**
+ * Wrapper of wc_get_template to relate directly to woocommerce-stripe
+ *
+ * @param string $template_name
+ * @return string
+ */
+function s4wc_get_template( $template_name ) {
+	return wc_get_template( $template_name, array(), WC()->template_path() . '/woocommerce-stripe', plugin_dir_path( __FILE__ ) . '/templates/' );
+}
